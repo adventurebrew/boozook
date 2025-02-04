e745e9578db8cf377617967432b0f52190ed77fc
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2025-02-04 18:14:39 +0100

    fix formatting o1_loadSpriteContent

--- src/boozook/codex/decomp_tot.py
+++ src/boozook/codex/decomp_tot.py
@@ -1147,9 +1147,7 @@ gob1_ops = { # version 49 - Gob1, Bargon, Fascination, LittleRed
     0x26: fparam('o1_createSprite', reads_uint16le, reads_uint16le, reads_uint16le, reads_uint16le),
     0x27: fparam('o1_freeSprite', reads_uint16le),
     0x30: fparam('o1_returnTo'),
-    0x31: fparam(
-        'o1_loadSpriteContent', reads_uint16le, reads_uint16le, reads_uint16le
-    ),
+    0x31: fparam('o1_loadSpriteContent', reads_uint16le, reads_uint16le, reads_uint16le),
     0x32: fparam(  # check diff in Fascination
         'o1_copySprite',
         reads_uint16le,
