b49296586cba89e13ad6420bb2399c25c77aa5c1
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2025-02-05 11:46:07 +0100

    add Adibou 2 Anglais opcodes

--- src/boozook/codex/decomp_tot.py
+++ src/boozook/codex/decomp_tot.py
@@ -1228,11 +1228,33 @@ gob6_ops = {  # version 52 - Playtoons, Adi4, Adibou2, Urban
     0x0B: xparam('oPlaytoons_printText'),
     0x11: xparam('o7_printText'),
 	0x19: xparam('o6_removeHotspot'),
+    0xA0: xparam('o7_draw0xA0', read_expr, read_var_index, read_expr),
+    0xAA: xparam('o7_draw0xAA', read_expr, read_expr)
+    0xAC: xparam('o7_draw0xAC', read_expr, read_expr, read_expr)
+    0xAD: xparam('o7_draw0xAD', read_expr, read_expr)
+    0xAF: xparam('o7_draw0xAF', read_expr, read_expr, read_expr)
+    0xB0: xparam('o7_draw0xB0', read_expr, read_expr, read_expr)
+    0xB1: xparam('07_draw0xB1', read_expr, read_expr)
+    0xB4: xparam('o7_draw0xB4', read_expr, read_expr, read_expr read_var_index)
+    0xB6: xparam('o7_draw0xB6', read_expr, read_expr, read_var_index)
+    0xCC: xparam('o7_draw0xCC', read_var_index)
+    0xCD: xparam('o7_draw0xCD', read_expr, read_var_index, read_var_index, read_var_index, read_var_index, read_var_index)
+    0xCE: xparam('07_draw0xCE', read_expr, read_var_index, read_var_index)
+    0xDC: xparam('o7_draw0xDC', read_expr, read_var_index)
+    0xDD: xparam('07_draw0xDD', read_expr)
+    0xDE: xparam('o7_draw0xDE', read_expr, read_expr)
+    0xDF: xparam('o7_draw0xDF', read_expr)
+    0xE0: xparam('07_draw0xE0')
+    0xE1: xparam('07_draw0xE1')
+    0xE2: xparam('07_draw0xE2', read_var_index, read_var_index, read_var_index)
+    0xE3: xparam('07_draw0xE3')
+    0xE4: xparam('07_draw0xE4')
     0x1B: xparam('oPlaytoons_F_1B'),
     0x24: xparam('oPlaytoons_putPixel'),
     0x27: xparam('oPlaytoons_freeSprite'),
 	0x32: xparam('o1_copySprite'),
 	0x33: xparam('o6_fillRect'),
+    0x34: xparam('o7_getFreeDiskSpace', read_uint16_le)
     # 0x33: xparam('o7_fillRect'),
     0x3F: xparam('oPlaytoons_checkData'),
     # 0x3F: xparam('o7_checkData'),
@@ -1241,6 +1263,7 @@ gob6_ops = {  # version 52 - Playtoons, Adi4, Adibou2, Urban
     0x3E: xparam('o7_getFreeMem'),
     0x4E: xparam('o7_writeData'),
     0x4D: xparam('oPlaytoons_readData'),
+    0x8C: xparam('o7_getImageFileInfo', read_expr, read_expr read_uint16_le read_uint16_le)
     # 0x4D: xparam('o7_readData'),
 }
 
