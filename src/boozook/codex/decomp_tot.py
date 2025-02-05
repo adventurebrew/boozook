8b62644225b215d0aef966cd7b137c444db011e9
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2025-02-05 18:46:31 +0100

    add explantion comment for opcode macros

--- src/boozook/codex/decomp_tot.py
+++ src/boozook/codex/decomp_tot.py
@@ -1,3 +1,8 @@
+# Opcode macros:
+# xparam - not implemented yet
+# gparam - function with custom logic regarding the script parsing
+# fparam - function that just read the given parametera by order
+
 from collections import defaultdict
 from contextlib import redirect_stdout
 from functools import partial
