9836e5574a1eba704462c8aca6269a84b1994b9c
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2025-03-03 12:43:24 +0100

    improve Script version mismatch output

--- src/boozook/codex/decomp_tot.py
+++ src/boozook/codex/decomp_tot.py
@@ -1603,7 +1603,7 @@ def main(gamedir, rebuild, scripts, lang=None, keys=False, exported=False):
         # TODO: could it be used to automatically detect optable
         prever = ctx.get('ver_script')
         if prever is not None and prever != tot_file[41]:
-            print('Warning: Script version mismatch', prever, tot_file[41])
+            print(f'Warning: Script version mismatch: {prever} ({tot_file[41]})')
         ctx['ver_script'] = tot_file[41]
         print('Script version', ctx['ver_script'], tot_file[0x3d])
 
