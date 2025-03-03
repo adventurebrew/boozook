86a21cd50196ad780c302468a4fcad2c4d244a77
Benjamin Funke <bjnfne@web.de>
2025-03-03 15:22:11 +0100

    Revert "merge opcodes to their proper section"
    This reverts commit d3a80da84c1f9ee94d483080103b5de6884b1796.

--- src/boozook/codex/decomp_tot.py
+++ src/boozook/codex/decomp_tot.py
@@ -1311,6 +1311,10 @@ gob3_ops = {  # version 51 - Gob3, Adibou1, Inca2, Woodruff, Dynasty
     0x23: fparam('o3_speakerOff'),
     0x25: fparam('oInca2_spaceShooter'),
     0x32: fparam('o3_copySprite', reads_uint16le, reads_uint16le, read_expr, read_expr, read_expr, read_expr, read_expr, read_expr, reads_uint16le),
+}
+
+gob4_ops = {
+    **gob3_ops,
     0x44: xparam('o4_draw0x44', read_expr, read_expr, read_expr, read_expr, read_expr, read_expr),
     0x45: xparam('o4_draw0x45', read_expr, read_expr),
     0x57: xparam('o4_draw0x57', read_var_index, read_var_index),
@@ -1321,6 +1325,11 @@ gob3_ops = {  # version 51 - Gob3, Adibou1, Inca2, Woodruff, Dynasty
     0xA1: xparam('o4_draw0xA1', read_expr, read_expr, read_expr, read_expr, read_var_index),
     0xA2: xparam('o4_draw0xA2', read_expr, read_expr, read_expr, read_expr),
     0xA4: xparam('o4_draw0xA4', read_expr, read_expr, read_expr),
+
+}
+
+gob5_ops = {
+    **gob4_ops,
     0x60: xparam('o5_deleteFile', read_expr),
     0x45: gparam('o5_istrlen'),
     0x00: gparam('o5_spaceShooter'),
@@ -1338,8 +1347,8 @@ gob3_ops = {  # version 51 - Gob3, Adibou1, Inca2, Woodruff, Dynasty
     0x0C: xparam('o5_gob98'),
     0x0D: xparam('o5_gob100', read_uint16le, read_uint16le, read_uint16le, read_uint16le),
     0x0E: xparam('o5_gob200', read_uint16le, read_uint16le, read_uint16le),
+    
 }
-
 
 gob6_ops = {  # version 52 - Playtoons, Adi4, Adibou2, Urban
     **gob3_ops,
